dea3e0b0693bafb2e0fd2419c89beae124754537
woakes070048 <woakes070048@gmail.com>
2015-07-06 17:39:34 -0400

    Update OpenProject-install.sh

--- OpenProject-install.sh
+++ OpenProject-install.sh
@@ -75,8 +75,6 @@ su - openproject -c "echo $SCRIPT | bash -l"
 # Grant permission to Passenger
 chmod o+x "/opt/openproject" 
 
-cd /opt/openproject/openproject/
-
 # Create the plugin gems file
 read -d '' PLUGINS <<"EOF"
 gem "pdf-inspector", "~>1.0.0", :group => :test
