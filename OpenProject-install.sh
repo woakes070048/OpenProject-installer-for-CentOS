d6cd25c420343aa462b470e937d12303e4cd8246
woakes070048 <woakes070048@gmail.com>
2015-07-06 17:08:29 -0400

    Update OpenProject-install.sh

--- OpenProject-install.sh
+++ OpenProject-install.sh
@@ -40,7 +40,7 @@ yum --assumeyes install \
 # Create dedicate user and group
 echo Step 3: Create user and group for openproject
 groupadd openproject
-useradd --create-home --gid openproject openproject
+useradd -d /opt/openproject -g openproject openproject
 echo "openproject\nopenproject" | (passwd --stdin openproject)
 
 # Create the script to run as openproject (Part 1)
@@ -51,6 +51,7 @@ read -d '' SCRIPT <<"EOF"
 	source ~/.profile
 
 	# Install RVM (Ruby Version Manager)
+	gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 	curl --silent --location https://get.rvm.io | bash -s stable
 	source $HOME/.rvm/scripts/rvm
 
@@ -76,15 +77,26 @@ echo Step 4: Install OpenProject - part 1
 su - openproject -c "echo $SCRIPT | bash -l"
 
 # Grant permission to Passenger
-chmod o+x "/home/openproject" 
+chmod o+x "/opt/openproject" 
 
 # Create the plugin gems file
 read -d '' PLUGINS <<"EOF"
 gem "pdf-inspector", "~>1.0.0", :group => :test
-gem "openproject-meeting", :git => "https://github.com/finnlabs/openproject-meeting.git", :branch => "stable" 
-gem "openproject-pdf_export", git: "https://github.com/finnlabs/openproject-pdf_export.git", :branch => "stable" 
-gem "openproject-plugins", git: "https://github.com/opf/openproject-plugins.git", :branch => "stable" 
-gem "openproject-backlogs", git: "https://github.com/finnlabs/openproject-backlogs.git", :branch => "stable" 
+gem "openproject-backlogs", :git => "https://github.com/finnlabs/openproject-backlogs.git", :branch => 'stable'
+gem "openproject-pdf_export", :git => "https://github.com/finnlabs/openproject-pdf_export.git", :branch => 'stable'
+gem 'openproject-costs', :git => 'https://github.com/finnlabs/openproject-costs.git', :branch => 'stable'
+gem 'openproject-global_roles', :git => 'https://github.com/finnlabs/openproject-global_roles.git', :branch => 'stable'
+gem 'openproject-help_link', :git => 'https://github.com/finnlabs/openproject-help_link.git', :branch => 'stable'
+gem 'openproject-meeting', :git => 'https://github.com/finnlabs/openproject-meeting.git', :branch => 'stable'
+gem 'openproject-my_project_page', :git => 'https://github.com/finnlabs/openproject-my_project_page.git', :branch => 'stable'
+gem 'openproject-documents', :git => 'https://github.com/opf/openproject-documents.git', :branch => 'stable'
+gem 'reporting_engine', :git => 'https://github.com/finnlabs/reporting_engine.git', :branch => 'dev'
+gem 'openproject-reporting', :git => 'https://github.com/finnlabs/openproject-reporting.git', :branch => 'stable'
+gem 'openproject-translations', :git => 'https://github.com/opf/openproject-translations.git', :branch => 'stable/4.1'
+gem "openproject-auto_project", :git => "https://github.com/oliverguenther/openproject-auto_project.git", :branch => 'stable'
+gem 'openproject-emoji',:git => 'https://github.com/tessi/openproject-emoji.git', :branch => 'stable'
+gem "openproject-ensure_project_hierarchy", :git => "https://github.com/oliverguenther/openproject-ensure_project_hierarchy.git", :branch => 'stable'
+gem "openproject-gitlab", git: "https://github.com/nietonfir/openproject-gitlab_integration.git", :branch=> "master" 
 EOF
 
 echo Step 5: Create Gemfile.plugins
@@ -113,16 +125,16 @@ echo Step 6: Install OpenProject - part 2
 su - openproject -c "echo $SCRIPT | bash -l"
 
 read -d '' CONF <<"EOF"
-LoadModule passenger_module /home/openproject/.rvm/gems/ruby-2.1.0/gems/passenger-4.0.37/buildout/apache2/mod_passenger.so
+LoadModule passenger_module /opt/openproject/.rvm/gems/ruby-2.1.0/gems/passenger-4.0.37/buildout/apache2/mod_passenger.so
 <IfModule mod_passenger.c>
-	PassengerRoot /home/openproject/.rvm/gems/ruby-2.1.0/gems/passenger-4.0.37
-	PassengerDefaultRuby /home/openproject/.rvm/gems/ruby-2.1.0/wrappers/ruby
+	PassengerRoot /opt/openproject/.rvm/gems/ruby-2.1.0/gems/passenger-4.0.37
+	PassengerDefaultRuby /opt/openproject/.rvm/gems/ruby-2.1.0/wrappers/ruby
 </IfModule>
 <VirtualHost *:80>
 	ServerName www.myopenprojectsite.com
 	# !!! Be sure to point DocumentRoot to 'public'!
-	DocumentRoot /home/openproject/openproject/public    
-	<Directory /home/openproject/openproject/public>
+	DocumentRoot /opt/openproject/openproject/public    
+	<Directory /opt/openproject/openproject/public>
 		# This relaxes Apache security settings.
 		AllowOverride all
 		# MultiViews must be turned off.
